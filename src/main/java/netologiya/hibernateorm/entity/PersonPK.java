fc15d03dfe0c7fb226a95ac836245fa746caf510
Simon Polushkin <105408053+SimonIsNotAvailable@users.noreply.github.com>
2023-03-22 09:16:17 +0500

    PersonPK.java added

---
+++ src/main/java/netologiya/hibernateorm/entity/PersonPK.java
@@ -0,0 +1,32 @@
+  public class PersonPK {
+    
+    private String name;
+    
+    private String surname;
+    
+    private int age;
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+     public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+  }
