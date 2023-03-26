30764910e4d6760ef74c7d81c442c865870cfc19
SimonIsNotAvailable <polushkinsn@gmail.com>
2023-03-26 19:49:39 +0500

    Security added

--- src/main/java/netologiya/hibernateorm/config/SecurityConfig.java
+++ src/main/java/netologiya/hibernateorm/config/SecurityConfig.java
@@ -1,18 +1,27 @@
 package netologiya.hibernateorm.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//    @Autowired
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+    auth.inMemoryAuthentication().withUser("user").password("password").roles("USER");
+}
+
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .withUser("Nikolashka")
                 .password("{noop}Nikolashka")
@@ -28,7 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-//    @Autowired
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin().and()
                 .authorizeRequests()
